2dc55b9953bc91741a6eabba8cce3c6aa3edb834
samarth <samarthk@thrillophilia.com>
2022-08-05 19:01:41 +0530

    erb app init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
-class ApplicationController < ActionController::API
-  # before_action :ensure_json_request  
-
-  # def ensure_json_request  
-  #   puts request.format
-  #   return if request.format == :json
-  #   render :nothing => true, :status => 406  
-  # end  
+class ApplicationController < ActionController::Base
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-
-
-  namespace :api, constraints: { format: 'json' } do 
-    namespace :v1 do
-      resources :authors
-      resources :blogs 
-      
-      get '/blogs/:id/comments', to: 'blogs#get_comments'
-      get '/blogs/:id/likes', to: 'blogs#get_likes'
-    end
-  end
+  # root "articles#index"
 end
