e1de6b36fffb8303406428a3e3903db19602895e
samarth <samarthk@thrillophilia.com>
2022-08-05 19:25:42 +0530

    workspace formatted

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -5,19 +5,19 @@ module Api
         @authors = Author.all
         render json: @authors
       end
-    
+
       def show
         @author = Author.find(params[:id])
         render json: @author
       end
-    
+
       def new
         @author = Author.new
       end
-    
+
       def create
         @author = Author.new(author_params)
-    
+
         respond_to do |format|
           if @author.save
             format.json { render :show, status: :created, location: @author }
@@ -26,12 +26,12 @@ module Api
           end
         end
       end
-    
+
       private
-      def author_params 
+
+      def author_params
         params.require(:author).permit(:username, :email)
       end
-    
     end
   end
 end
--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,48 +1,47 @@
 module Api
   module V1
     class BlogsController < ApplicationController
-
       before_action :find_blog, only: %i[show get_comments get_likes]
       before_action :set_default_response_format
 
       def index
         puts request.format
         @blogs = Blog.order(updated_at: :desc).limit(100)
-        
-        
+
         render json: @blogs.map(&:attributes)
       end
-    
+
       def show
         render json: @blog
       end
-    
+
       def create
         @blog = Blog.new(blog_params)
-    
+
         respond_to do |format|
           if @blog.save
             format.json { render :show, status: :created, location: @blog }
           else
-            format.json {render json: @blog.errors, status: :unprocessable_entity}
+            format.json { render json: @blog.errors, status: :unprocessable_entity }
           end
         end
       end
-    
+
       def get_comments
-       render json: @blog.comments
+        render json: @blog.comments
       end
-    
+
       def get_likes
         render json: @blog.likes
       end
-    
+
       private
+
       def find_blog
         @blog = Blog.find(params[:id])
         raise 'Blog Id not found' if @blog.blank?
       end
-    
+
       def blog_params
         params.require(:blog).permit(:title, :content, :author_id)
       end
@@ -50,10 +49,6 @@ module Api
       def set_default_response_format
         request.format = :json
       end
-
-
     end
   end
 end
-
-
